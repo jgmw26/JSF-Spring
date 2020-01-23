2a334a4d55ad6fade83e1f5f842f8a337db00138
jgmw26 <joaomeira26.jg@gmail.com>
2020-01-23 14:30:21 -0300

    first commit

---
+++ src/main/java/br/com/qintess/livraria/bean/MenuBean.java
@@ -0,0 +1,13 @@
+package br.com.qintess.livraria.bean;
+
+import java.io.Serializable;
+
+import org.springframework.stereotype.Controller;
+
+@SuppressWarnings("serial")
+@Controller
+public class MenuBean implements Serializable {
+
+	
+	
+}
